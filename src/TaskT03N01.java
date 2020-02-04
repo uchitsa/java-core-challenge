053b07d07b0d1f35e3d39617d53828be1d2f2324
uchitsa <uchitsa@ya.ru>
2020-02-04 13:26:54 +0300

    Taskt03N01 in work

---
+++ src/TaskT03N01.java
@@ -0,0 +1,22 @@
+package src;
+
+import java.util.Scanner;
+
+public class TaskT03N01 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the string:");
+        String s = scan.nextLine();
+        System.out.println("Enter № of char:");
+        int n = scan.nextInt();
+        if (n < s.length())
+            System.out.println(getNthSymbol(s, n));
+        else
+            System.out.println("Uncorrect № of char");
+    }
+
+    static char getNthSymbol(String s, int n) {
+        return s.charAt(n);
+    }
+}
